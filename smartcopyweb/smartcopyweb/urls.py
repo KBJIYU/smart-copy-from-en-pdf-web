c4cafc9302e175b2100d1e61e862e5518ad669f5
kbjiyu <KBJIYU@users.noreply.github.com>
2019-03-26 23:52:48 +0800

    remove smartapi folder

--- smartcopyweb/smartcopyweb/urls.py
+++ smartcopyweb/smartcopyweb/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('testapp/', include('testapp.urls')),
     path('', include('frontend.urls')),
-    # path('api/', include('smartapi.urls'))
 ]
